07b343a0318ab8e1388495414cf7f4cbfe852339
muhibbx5 <muhibbali56@gmail.com>
2024-10-25 11:44:03 +0500

    birthday

---
+++ main.py
@@ -0,0 +1,14 @@
+# Storing birthday dates in variables
+birthday1 = "1995-01-15"
+birthday2 = "1988-06-23"
+birthday3 = "2000-12-05"
+birthday4 = "1992-03-17"
+birthday5 = "1985-11-30"
+
+# Printing the birthdays
+print("Birthdays:")
+print("1:", birthday1)
+print("2:", birthday2)
+print("3:", birthday3)
+print("4:", birthday4)
+print("5:", birthday5)
